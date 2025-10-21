70051c9475174a117bdc947732c6300d80148db5
HarshRaut9945 <harshraut9945@gmail.com>
2025-10-21 14:41:14 +0530

    add

---
+++ Concrete_pred/app.py
@@ -0,0 +1,20 @@
+from flask import Flask,request,render_template
+
+import numpy as np
+import pandas as pd
+import pickle 
+
+#create flask app
+app=Flask(__name__)
+
+#route
+
+
+
+
+
+
+
+#pythoon main
+if __name__=="__main__":
+    app.run(debug=True)
